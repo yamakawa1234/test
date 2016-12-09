09d9da040bc66800ddbc93dc912bc408a87ca63a
yamakawa <yamakawa@cbase.co.jp>
2016-12-09 10:10:07 +0900

    contain bug source file

---
+++ mysql_connect_bug.php
@@ -0,0 +1,25 @@
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+
+<?php
+ini_set( 'display_errors', 1 );
+print('<p>successed</p>');
+$link = mysql_connect('localhost', 'root', 'root');
+if (!$link) {
+    die('failed'.mysql_error());
+}
+
+print('<p>successed</p>');
+
+// MySQLに対する処理
+
+$close_flag = mysql_close($link);
+
+if ($close_flag){
+    print('<p>切断に成功しました。</p>');
+// if文の後ろ括弧削除
+
+?>
+</body>
+</html>
